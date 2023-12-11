ff869feb6245a8849cbe5098da8a88a1a27993bc
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2023-12-11 16:43:57 +0100

    add llama2-zeroshot

---
+++ app/nshot/llama2_zero_shot.py
@@ -0,0 +1,66 @@
+import os
+from huggingface_hub import InferenceClient, login
+from argparse import ArgumentParser
+from transformers import AutoTokenizer
+import pandas as pd
+from app.nshot.json_extractor import json_extractor
+from dotenv import load_dotenv
+
+load_dotenv()
+# access token with permission to access the model and PRO subscription
+hf_token = os.getenv("HF_INFERENCE_TOKEN")  # https://huggingface.co/settings/tokens
+login(token=hf_token)
+
+# tokenizer for generating prompt
+tokenizer = AutoTokenizer.from_pretrained(os.getenv("HF_MODEL_TOKENIZER"))
+
+# inference client
+client = InferenceClient(os.getenv("HF_MODEL"))
+
+
+# generate function
+def generate(text):
+    payload = tokenizer.apply_chat_template([{"role": "user", "content": text}], tokenize=False)
+    res = client.text_generation(
+        payload,
+        do_sample=True,
+        return_full_text=False,
+        max_new_tokens=2048,
+        top_p=0.9,
+        temperature=0.6,
+    )
+    return res.strip()
+
+
+def read_prompt_file(prompt_file):
+    with open(prompt_file, 'r') as file:
+        prompt_msg = file.read()
+        return prompt_msg
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--prompt_file", type=str, required=True)
+
+    args = parser.parse_args()
+
+    llama2_prompt = """<s>[INST] <<SYS>>
+    {sys_message}
+    <</SYS>>
+    Text:
+    ```{sentence}``` [/INST]
+    """
+
+    sys_message = read_prompt_file(prompt_file=args.prompt_file)
+
+    samples = pd.read_csv("tasks/spot/v10/IMR_Dataset_v10_dev_ChatNL_minimized.csv", sep='\t').sample(5)
+
+    for idx, sample in samples.iterrows():
+        query = sample["sentence"]
+        generated_text = generate(llama2_prompt.format(sentence=query, sys_message=sys_message))
+        json_data = json_extractor(generated_text)
+
+        if json_data is not None:
+            json_data["sentence"] = query
+
+        print(json_data)
---
+++ scripts/spot/zero_shot_llama.sh
@@ -0,0 +1,4 @@
+echo zero shot prompt with simple prompt
+
+python -m app.nshot.llama2_zero_shot \
+--prompt_file scripts/spot/prompt/zero_shot_prompt.txt
