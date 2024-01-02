b13e4c1a8672a65648af261831d5135eba62b603
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-01-02 12:04:46 +0100

    update training script

--- app/models.py
+++ app/models.py
@@ -1,22 +1,3 @@
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments, AutoModelForSeq2SeqLM
+from app.t5_models import T5Model
 
-
-class T5Model:
-    def load(self, pretrained_model):
-        return AutoModelForSeq2SeqLM.from_pretrained(pretrained_model)
-
-    def training_args(self, **args):
-        return Seq2SeqTrainingArguments(**args)
-
-    def trainer(self, **args):
-        return Seq2SeqTrainer(**args)
-
-
-class LlamaModel:
-    pass
-
-
-MODELS = {
-    "t5": T5Model,
-    "llama": LlamaModel
-}
+MODELS = {"t5": T5Model}
