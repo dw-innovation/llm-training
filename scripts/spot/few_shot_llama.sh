9f3bda19c3de3593bb67977e7a9071c7072f5d22
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-02-21 14:45:21 +0100

    update data

--- scripts/spot/few_shot_llama.sh
+++ scripts/spot/few_shot_llama.sh
@@ -14,18 +14,18 @@ echo 1-shot prompt human prompt v2
 #done
 
 
-#echo 2-shot prompt human prompt v2
-#
-#for i in {1..10}
-#do
-#few_shot=2
-#model_name=few_shot_prompt_human_prompt_v2
-#python -m app.nshot.llama2_few_shot \
-#--few_shot $few_shot \
-#--prompt_file scripts/spot/prompt/${model_name}.txt \
-#--input_file tasks/spot/gold/sentences.txt \
-#--result_file ${OUTPUT_FOLDER}/gold_predictions_llama2_${model_name}_shot_${few_shot}_file_${i}.jsonl
-#done
+echo 2-shot prompt human prompt v2
+
+for i in {1..10}
+do
+few_shot=2
+model_name=few_shot_prompt_human_prompt_v2
+python -m app.nshot.llama2_few_shot \
+--few_shot $few_shot \
+--prompt_file scripts/spot/prompt/${model_name}.txt \
+--input_file tasks/spot/gold/sentences.txt \
+--result_file ${OUTPUT_FOLDER}/gold_predictions_llama2_${model_name}_shot_${few_shot}_file_${i}.jsonl
+done
 
 
 #echo 5-shot prompt human prompt v2
@@ -42,16 +42,16 @@ echo 1-shot prompt human prompt v2
 #done
 
 
-echo 10-shot prompt human prompt v2
-
-for i in {1..10}
-do
-few_shot=10
-model_name=few_shot_prompt_human_prompt_v2
-python -m app.nshot.llama2_few_shot \
---few_shot $few_shot \
---prompt_file scripts/spot/prompt/${model_name}.txt \
---input_file tasks/spot/gold/sentences.txt \
---result_file ${OUTPUT_FOLDER}/gold_predictions_llama2_${model_name}_shot_${few_shot}_file_${i}.jsonl
-done
+#echo 10-shot prompt human prompt v2
+#
+#for i in {1..10}
+#do
+#few_shot=10
+#model_name=few_shot_prompt_human_prompt_v2
+#python -m app.nshot.llama2_few_shot \
+#--few_shot $few_shot \
+#--prompt_file scripts/spot/prompt/${model_name}.txt \
+#--input_file tasks/spot/gold/sentences.txt \
+#--result_file ${OUTPUT_FOLDER}/gold_predictions_llama2_${model_name}_shot_${few_shot}_file_${i}.jsonl
+#done
 
