5578fd8dc0eda1e7e3e651c0a64f1bb5d588411b
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-11-29 14:43:39 +0100

    update prompt

--- app/nshot/chatgpt_zero_shot.py
+++ app/nshot/chatgpt_zero_shot.py
@@ -1,30 +1,69 @@
 import os
 import json
+from enum import Enum
 from tqdm import tqdm
 from argparse import ArgumentParser
 from app.nshot.json_extractor import json_extractor
 from openai import OpenAI
 from dotenv import load_dotenv
 from loguru import logger
+from pydantic import BaseModel
+from typing import List, Optional
 from app.nshot.utils import read_prompt_file
 
 load_dotenv()
-def generate(client, query, prompt):
+
+class EntityType(Enum):
+    NWR = 'nwr'
+    CLUSTER = 'cluster'
+
+class RelationType(Enum):
+    DIST = 'dist'
+    CONTAINS = 'contains'
+
+class Area(BaseModel):
+    type: str
+    value: Optional[str]
+
+class Property(BaseModel):
+    name: str
+    operator: str
+    value: str
+
+class Entity(BaseModel):
+    name: str
+    id: str
+    type: EntityType
+    properties: List[Property]
+
+class Relation(BaseModel):
+    source: str
+    target: str
+    type: RelationType
+    value: Optional[str]
+
+class Output(BaseModel):
+    area: Area
+    entities: List[Entity]
+    relations: List[Relation]
+
+def generate(client, query, prompt, response_prompt):
     logger.info(f"Prompting {query}")
-    completion = client.chat.completions.create(
+    completion = client.beta.chat.completions.parse(
         model=os.getenv("CHATGPT_MODEL"),
         messages=[
             {"role": "system", "content": prompt},
             {"role": "user", "content": query}
         ],
-        temperature=0.6
+        temperature=0.6,
+        response_format=response_prompt
     )
     logger.info(f"chatgpt returned a result")
     raw_output = completion.choices[0].message.content
-    logger.info(f"the document is parsing")
-    parsed_output = json_extractor(raw_output)
-    logger.info(f"the document parsed")
-    return parsed_output, raw_output
+    # logger.info(f"the document is parsing")
+    # parsed_output = json_extractor(raw_output)
+    # logger.info(f"the document parsed")
+    return raw_output
 
 
 if __name__ == '__main__':
@@ -37,7 +76,7 @@ if __name__ == '__main__':
         args = parser.parse_args()
 
         sys_message = read_prompt_file(prompt_file=args.prompt_file)
-        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"], organization=os.environ["OPENAI_ORG"])
 
         with open(args.input_file, 'r') as input_file:
             sentences = input_file.readlines()
@@ -45,9 +84,13 @@ if __name__ == '__main__':
             with open(args.result_file, 'w') as outfile:
                 for sentence in tqdm(sentences, total=len(sentences)):
                     json_data = {"sentence": sentence}
-                    sentence = sentence.rstrip()
-                    generated_text = generate(client=client, query=sentence, prompt=sys_message)
+                    sentence = sentence.rstrip().lower()
+                    generated_text = generate(client=client, query=sentence, prompt=sys_message, response_prompt=Output)
+                    # if isinstance(generated_text, tuple):
+                    #     generated_text = generated_text
+                    generated_text = generated_text.replace('```yaml', '').replace('```', '')
+                    generated_text = generated_text.replace('*', '-').replace('+', '-')
                     json_data["model_result"] = generated_text
-                    json_data["parsed_result"] = json_extractor(generated_text)
-                    json.dump(json_data, outfile)
+                    # json_data["parsed_result"] = json_extractor(generated_text)
+                    json.dump(json_data, outfile, ensure_ascii=False)
                     outfile.write('\n')
--- scripts/spot/zero_shot_chatgpt.sh
+++ scripts/spot/zero_shot_chatgpt.sh
@@ -4,14 +4,16 @@ echo zero shot prompt with simple prompt
 
 mkdir -p $OUTPUT_FOLDER
 
-python -m app.nshot.chatgpt_zero_shot \
---prompt_file scripts/spot/prompt/zero_shot_prompt.txt \
---input_file tasks/spot/gold/sentences.txt \
---result_file ${OUTPUT_FOLDER}/gold_predictions_chatgpt_zeroshot.jsonl
+#python -m app.nshot.chatgpt_zero_shot \
+#--prompt_file scripts/spot/prompt/zero_shot_prompt.txt \
+#--input_file tasks/spot/gold/sentences.txt \
+#--result_file ${OUTPUT_FOLDER}/gold_predictions_chatgpt_zeroshot.jsonl
 #
 echo zero shot prompt with simple cot prompt
+
+MODEL_NAME=gpt-4o
 
 python -m app.nshot.chatgpt_zero_shot \
 --prompt_file scripts/spot/prompt/zero_shot_cot_prompt.txt \
 --input_file tasks/spot/gold/sentences.txt \
---result_file ${OUTPUT_FOLDER}/gold_predictions_chatgpt_cot_zeroshot.jsonl
+--result_file ${OUTPUT_FOLDER}/gold_predictions_${MODEL_NAME}_cot_zeroshot_2.jsonl
